2b67a65473113b48039da1de6678dc1a7a37159e
asawyerskofax <adam.sawyers@kofax.com>
2021-04-30 10:20:30 -0700

    Add project files.

---
+++ Debugger/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using SalesforceApiLib;
+
+namespace Debugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string client_id = "3MVG9Eroh42Z9.iVnYylfw221tNItLyCkYT9V5SqGISPVMy8cSl0FsYhj13Pieh1TiYPJB7m3L_GF9jLFN.jw";
+            string client_secret = "BE94011CC2E7144E9B7461E5EE56AF7DD25C8DADA8260CD7615193540B68CCB5";
+            string username = "adam.sawyers18@kofax.com.strategy";
+            string password = "Hippomilk-00";
+            string token = "";
+
+            string acctName = "Adams Adventures";
+            string pricebookName = "Adams Test Pricebook";
+            string pricebookEntry = "Adams Test Product";
+
+            var client = new SalesforceApi();
+
+            Console.WriteLine("Logging in");
+            var loginResponse = client.Login(client_id, client_secret, username, password, token, false);
+
+            Console.WriteLine("Write client object AuthToken prop");
+            Console.WriteLine(loginResponse[0]);
+            string authToken = loginResponse[0];
+
+            Console.WriteLine("Write client object InstanceUrl prop");
+            Console.WriteLine(loginResponse[1]);
+            string instanceUrl = loginResponse[1];
+
+            Console.WriteLine("Query Salesforce Accounts");
+            //var queryResult = client.Query(instanceUrl, authToken, "SELECT Name,Id FROM Account WHERE Name='Liz Inc.'");
+
+            var queryAcctResult = client.Query(instanceUrl, authToken, $"SELECT Name,Id FROM Account WHERE Name='{acctName}'");
+            Console.WriteLine(queryAcctResult);
+
+            var queryPricebookResult = client.Query(instanceUrl, authToken, $"SELECT Name,Id FROM Pricebook2 WHERE Name='{pricebookName}'");
+            Console.WriteLine(queryPricebookResult);
+
+            var queryPricebookEntryResult = client.Query(instanceUrl, authToken, $"SELECT Name,Id FROM PricebookEntry WHERE Name='{pricebookEntry}'");
+            Console.WriteLine(queryPricebookEntryResult);
+
+            //pull the ids out of each query, too lazy to do it here (also unecessary since KTA will do it)
+            var acctId = "00102000008UmtiAAC";
+            var pricebookId = "01s02000000DahoAAC";
+            var pricebookEntryId = "01u020000017GBOAA2";
+
+            var addOrderResult = client.AddOrder(instanceUrl, authToken, "1", "10", "2021-04-30", "San Diego", acctId, pricebookId, pricebookEntryId);
+            Console.WriteLine(addOrderResult);
+
+            Console.ReadLine();            
+        }
+    }
+}
---
+++ SalesforceApiLib/SalesforceApi.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SalesforceApiLib
+{
+    public class SalesforceApi
+    {
+        private const string TEST_LOGIN_ENDPOINT = "https://test.salesforce.com/services/oauth2/token";
+        private const string PROD_LOGIN_ENDPOINT = "https://login.salesforce.com/services/oauth2/token";
+        private const string API_ENDPOINT = "/services/data/v51.0/";
+
+        static SalesforceApi()
+        {
+            // SF requires TLS 1.1 or 1.2
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+        }
+
+        // TODO: use RestSharps
+        public string[] Login(string clientId, string clientSecret, string username, string password, string token, bool prodInstance)
+        {
+            String jsonResponse;
+            using (var client = new HttpClient())
+            {
+                var request = new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                        {"grant_type", "password"},
+                        {"client_id", clientId},
+                        {"client_secret", clientSecret},
+                        {"username", username},
+                        {"password", password + token}
+                    }
+                );
+
+                request.Headers.Add("X-PrettyPrint", "1");
+
+                //added if statement to handle test/prod
+                var LOGIN_ENDPOINT = TEST_LOGIN_ENDPOINT;
+                if (prodInstance == true)
+                {
+                    LOGIN_ENDPOINT = PROD_LOGIN_ENDPOINT;
+                }
+
+                var response = client.PostAsync(LOGIN_ENDPOINT, request).Result;
+                jsonResponse = response.Content.ReadAsStringAsync().Result;
+            }
+            Console.WriteLine($"Response: {jsonResponse}");
+            var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonResponse);
+            string[] loginInfoArr = { values["access_token"], values["instance_url"] };
+            return loginInfoArr;
+        }
+
+        public string Query(string instanceUrl, string authToken, string soqlQuery)
+        {
+            using (var client = new HttpClient())
+            {
+                string restRequest = instanceUrl + API_ENDPOINT + "query/?q=" + soqlQuery;
+                var request = new HttpRequestMessage(HttpMethod.Get, restRequest);
+                request.Headers.Add("Authorization", "Bearer " + authToken);
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.Add("X-PrettyPrint", "1");
+                var response = client.SendAsync(request).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        public string AddOrder(string instanceUrl, string authToken, string quantity, string unitPrice, string effectiveDate, string billingCity, string accountId, string pricebookId, string pricebookEntryId)
+        {
+            //create attributes object for the order
+            var requestBodyOrderAttributes = new Attributes
+            {
+                type = "Order"
+            };
+
+            //create attributes object for order item
+            var requestBodyOrderItemAttributes = new Attributes
+            {
+                type = "OrderItem"
+            };
+
+            //create order item record object
+            //var requestBodyOrderItemRecord = new OrderItemRecords
+            //{
+            //    Attributes = requestBodyOrderItemAttributes,
+            //    PricebookEntryId = pricebookEntryId,
+            //    Quantity = "1",
+            //    UnitPrice = "10"
+            //};
+
+            //create records array of order items
+            var requestBodyOrderRecord = new List<OrderItemRecords>
+            {
+                new OrderItemRecords {
+                attributes = requestBodyOrderItemAttributes,
+                PricebookEntryId = pricebookEntryId,
+                quantity = quantity,
+                UnitPrice = unitPrice
+                },
+            };
+
+            //create order items from that array of order items
+            var requestBodyOrderItems = new OrderItems
+            {
+                records = requestBodyOrderRecord
+            };
+
+            //create order
+            //var requestBodyOrder = new Order
+            //{
+            //    Attributes = requestBodyOrderAttributes,
+            //    EffectiveDate = "2021-04-07",
+            //    Status = "Draft",
+            //    BillingCity = "San Diego",
+            //    AccountId = accountId,
+            //    Pricebook2Id = pricebookId,
+            //    OrderItems = requestBodyOrderItems
+            //};
+
+            //create array of orders
+
+            //create array of orders,
+            var requestBodyOrderArray = new List<Order>
+            {
+                new Order {
+                attributes = requestBodyOrderAttributes,
+                EffectiveDate = effectiveDate,
+                Status = "Draft",
+                billingCity = billingCity,
+                accountId = accountId,
+                Pricebook2Id = pricebookId,
+                OrderItems = requestBodyOrderItems
+                },
+            };
+
+            //add the order, then serialize
+            var requestBody = new OrderArray
+            {
+                order = requestBodyOrderArray,
+            };
+            Console.WriteLine("Created request body");
+            Console.WriteLine("Serializing request body");
+            var requestBodyStr = JsonConvert.SerializeObject(requestBody);
+            Console.WriteLine(requestBodyStr);
+
+            //String jsonResponse;
+            //using (var client = new HttpClient())
+            //{
+
+            //    var request = new FormUrlEncodedContent(new Dictionary<string, string>
+            //    {
+            //        { "order", requestBodyStr }
+            //    });
+            //    request.Headers.Add("Authorization", "Bearer " + authToken);
+            //    request.Headers.Add("X-PrettyPrint", "1");
+            //    var response = client.PostAsync(instanceUrl + API_ENDPOINT, request).Result;
+            //    jsonResponse = response.Content.ReadAsStringAsync().Result;
+            //}
+
+            Console.WriteLine(instanceUrl + API_ENDPOINT + "commerce/sale/order");
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, instanceUrl + API_ENDPOINT + "commerce/sale/order");
+            request.Headers.Add("Authorization", "Bearer " + authToken);
+
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Content = new StringContent(requestBodyStr, Encoding.UTF8, "application/json");
+            HttpClient postClient = new HttpClient();
+
+            var result = postClient.SendAsync(request).Result;
+            var jsonResponse = result.Content.ReadAsStringAsync().Result;
+
+            //var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonResponse);
+            return jsonResponse;
+        }
+    }
+
+    public class OrderArray
+    {
+        public List<Order> order { get; set; }
+    }
+
+    public class Order
+    {
+        public Attributes attributes { get; set; }
+        //[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+
+        public string EffectiveDate { get; set; }
+        //[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+
+        public string Status { get; set; }
+        //[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+
+        public string billingCity { get; set; }
+        //[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+
+        public string accountId { get; set; }
+        //[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+
+        public string Pricebook2Id { get; set; }
+        //[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+
+        public OrderItems OrderItems { get; set; }
+        //[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    }
+
+    public class Attributes
+    {
+        public string type { get; set; }
+    }
+
+    public class OrderItems
+    {
+        public List<OrderItemRecords> records { get; set; }
+    }
+
+    public class OrderItemRecords
+    {
+        public Attributes attributes { get; set; }
+        public string PricebookEntryId { get; set; }
+        public string quantity { get; set; }
+        public string UnitPrice { get; set; }
+    }
+
+    //public string Describe(string instanceUrl, string apiEndpoint, string authToken, string sObject)
+    //{
+    //    using (var client = new HttpClient())
+    //    {
+    //        string restQuery = instanceUrl + API_ENDPOINT + "sobjects/" + sObject;
+    //        var request = new HttpRequestMessage(HttpMethod.Get, restQuery);
+    //        request.Headers.Add("Authorization", "Bearer " + authToken);
+    //        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    //        request.Headers.Add("X-PrettyPrint", "1");
+    //        var response = client.SendAsync(request).Result;
+    //        return response.Content.ReadAsStringAsync().Result;
+    //    }
+    //}
+
+    //public string QueryEndpoints(string instanceUrl, string apiEndpoint, string authToken)
+    //{
+    //    using (var client = new HttpClient())
+    //    {
+    //        string restQuery = instanceUrl + API_ENDPOINT;
+    //        var request = new HttpRequestMessage(HttpMethod.Get, restQuery);
+    //        request.Headers.Add("Authorization", "Bearer " + authToken);
+    //        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    //        request.Headers.Add("X-PrettyPrint", "1");
+    //        var response = client.SendAsync(request).Result;
+    //        return response.Content.ReadAsStringAsync().Result;
+    //    }
+    //}
+}
+
+//var orderArray = new[] {
+//    new {
+//        attributes = new { type = "Order"},
+//        EffectiveDate = "2021-04-07",
+//        Status = "Draft",
+//        billingCity = "San Diego",
+//        accountId = accountId,
+//        Pricebook2Id = pricebookId,
+//        OrderItems = new {
+//            records = new[] {
+//                new {
+//                    attributes = new { type = "OrderItem" },
+//                    PricebookEntryId = pricebookEntryId,
+//                    quantity = 1,
+//                    UnitPrice = 10
+//                }
+//            }
+//        }
+//    }
+//};
+//string orderStr = JsonConvert.SerializeObject(orderArray, Formatting.Indented);
+
+//dynamic order = new dynamic[1];
+//order[0] = new ExpandoObject();
+//order[0].attributes = new ExpandoObject();
+//order[0].attributes.type = "Order";
+//order[0].EffectiveDate = "2021-04-08";
+//order[0].Status = "Draft";
+//order[0].billingCity = "San Diego";
+////order[0].accountId = "00102000008UmtiAAC";
+//order[0].accountId = accountId;
+
+////order[0].Pricebook2Id = "01s02000000Daho";
+//order[0].Pricebook2Id = pricebookId;
+
+//order[0].OrderItems = new ExpandoObject();
+//order[0].OrderItems.records = new dynamic[1];
+//order[0].OrderItems.records[0] = new ExpandoObject();
+//order[0].OrderItems.records[0].attributes.type = "OrderItem";
+
+////order[0].OrderItems.records[0].PricebookEntryId = "01u020000017GBO";
+//order[0].OrderItems.records[0].PricebookEntryId = pricebookEntryId;
+//order[0].OrderItems.records[0].quantity = "1";
+//order[0].OrderItems.records[0].UnitPrice = 10.00;
+//string json = JsonConvert.SerializeObject(order, Formatting.Indented);
